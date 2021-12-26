b8ff6cda2643b61c46f0c6864aa71b9f28b4f468
Neema Samawi <neemasamawi@wustl.edu>
2021-12-25 21:30:30 -0500

    fixed spacing and added fade effect

---
+++ JS/homepage.js
@@ -0,0 +1,63 @@
+function() {
+  $("#main-link").on('click', function() {
+    var position = $("#main-covers").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+function() {
+  $("#illustration-link").on('click', function() {
+    var position = $("#illustration").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+function() {
+  $("#illustration-preview").on('click', function() {
+    var position = $("#illustration").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+
+function() {
+  $("#drawing-link").on('click', function() {
+    var position = $("#drawing").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+function() {
+  $("#drawing-preview").on('click', function() {
+    var position = $("#drawing").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+function() {
+  $("#art-link").on('click', function() {
+    var position = $("#art").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
+
+function() {
+  $("#art-preview").on('click', function() {
+    var position = $("#art").offset().top;
+    $("HTML, BODY").animate({
+      scrollTop: position
+    }, 750);
+  });
+};
